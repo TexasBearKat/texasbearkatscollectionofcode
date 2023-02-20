ec79349f99a8bd28418161925db8b4b2cb15ac2c
TexasBearKat <125840338+TexasBearKat@users.noreply.github.com>
2023-02-19 20:23:27 -0600

    Add files via upload
    File to show my friends. It is a word guessing game coded in C#.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {string secretWord = "secret";
+            string guess = "";
+            do{
+                Console.WriteLine("\nGuess: ");
+                guess = Console.ReadLine();
+            }while (secretWord != guess);    
+            Console.WriteLine("You win!");}            
+    }
+} 
+
+
+
+
+
